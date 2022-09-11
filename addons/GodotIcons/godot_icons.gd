e17a970665be4d60c573efd8106a35ab185a35af
Axsajim Reyes <axsajimreyes@gmail.com>
2022-09-11 08:07:56 -0400

    keyboard input working

--- addons/GodotIcons/godot_icons.gd
+++ addons/GodotIcons/godot_icons.gd
@@ -2,34 +2,30 @@ extends Node
 class_name GodotIcons
 
 
-var inputThemes = ["Blanks", "Dark", "Light"]
+static func input(key: String):
+	
+	var key_params = key.split("_", false)
 
-static func input(binding: String):
+	print(key_params)
 	
-	var capitalized = binding.capitalize()
+	var binding = str(key_params[0]).capitalize()
 	
-	var key_params = capitalized.split("_")
-
-	print(key_params.size())
+	var type = str(key_params[1]).capitalize()
 	
-	var key = str(key_params[0])
-	
-	var type = str(key_params[1])
-	
-	var theme = str(key_params[2])
+	var theme = str(key_params[2]).capitalize()
 
 	var directory = "res://addons/GodotIcons/media/assets/XeluControllerAndKeyPrompts/"
 
-	var resource_directory = str(directory + type + "/" + theme + "/" + key + "_Key_" + theme + ".png")
+	var resource_directory = str(directory + type + "/" + theme + "/" + binding + "_Key_" + theme + ".png")
 
-	#print(resource_directory)
+	print(resource_directory)
 	
 	var x_directory = "res://addons/GodotIcons/media/assets/XeluControllerAndKeyPrompts/KeyboardAndMouse/Light/A_Key_Light.png"
 
 
 	var icon = load(resource_directory)
 
-	if !binding:
+	if !key:
 		return null
 	
 	else:
